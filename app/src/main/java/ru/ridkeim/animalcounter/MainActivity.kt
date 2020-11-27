7fa2d8194b44082d54226b8ec69e12f5a2e76de5
ridkeim <ridkeim@gmail.com>
2020-11-27 17:46:48 +0300

    Реализовано сохранение данный в SharedPreferences и добавлена локализация(ru)

--- app/src/main/java/ru/ridkeim/animalcounter/MainActivity.kt
+++ app/src/main/java/ru/ridkeim/animalcounter/MainActivity.kt
@@ -3,29 +3,21 @@ package ru.ridkeim.animalcounter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import kotlinx.android.synthetic.main.activity_main.*
 import ru.ridkeim.animalcounter.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MainActivityViewModel
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView( this, R.layout.activity_main)
-        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView( this, R.layout.activity_main)
+        val viewModel: MainActivityViewModel = ViewModelProvider(
+            this,
+            MainActivityViewModel.Factory(application)
+        ).get(MainActivityViewModel::class.java)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
-        viewModel.catCount.observe(this) {
-            catTextView.text = resources.getQuantityString(R.plurals.numberOfCats, it, it)
-        }
-        viewModel.crowCount.observe(this) {
-            crowTextView.text = resources.getQuantityString(R.plurals.numberOfCrows, it, it)
-        }
+        lifecycle.addObserver(viewModel)
     }
-
 }
--- app/src/main/java/ru/ridkeim/animalcounter/MainActivityViewModel.kt
+++ app/src/main/java/ru/ridkeim/animalcounter/MainActivityViewModel.kt
@@ -1,20 +1,47 @@
 package ru.ridkeim.animalcounter
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
+import androidx.lifecycle.*
+import java.lang.IllegalArgumentException
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel(application: Application) : AndroidViewModel(application), LifecycleObserver{
+
+    companion object {
+        private const val KEY_SETTINGS = "settings"
+        private const val KEY_CROW_COUNT = "crow_count"
+        private const val KEY_CAT_COUNT = "cat_count"
+    }
+
+    private val context : Context
+    private val prefs : SharedPreferences
     private val _crowCount = MutableLiveData<Int>()
-    val crowCount : LiveData<Int>
-    get() = _crowCount
+    val crowCount : LiveData<String>
+    get() = Transformations.map(_crowCount){
+        context.resources.getQuantityString(R.plurals.numberOfCrows, it, it)
+    }
     private val _catCount = MutableLiveData<Int>()
-    val catCount : LiveData<Int>
-    get() = _catCount
-
+    val catCount : LiveData<String>
+    get() = Transformations.map(_catCount){
+        context.resources.getQuantityString(R.plurals.numberOfCats, it, it)
+    }
     init {
-        _crowCount.value = 0
-        _catCount.value = 0
+        context = getApplication()
+        prefs = context.getSharedPreferences(KEY_SETTINGS,Context.MODE_PRIVATE)
+        _crowCount.value = prefs.getInt(KEY_CROW_COUNT,0)
+        _catCount.value = prefs.getInt(KEY_CAT_COUNT,0)
+    }
+
+    class Factory(private val application: Application) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if(modelClass.isAssignableFrom(MainActivityViewModel::class.java)){
+                return MainActivityViewModel(application) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
     }
 
     fun onCatFound(){
@@ -23,6 +50,26 @@ class MainActivityViewModel : ViewModel() {
 
     fun onCrowFound(){
         _crowCount.value = _crowCount.value?.plus(1)
+    }
+
+    fun resetCounters(){
+        _crowCount.value = 0
+        _catCount.value = 0
+        saveCounters()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun saveCounters(){
+        val prefsEditor = prefs.edit()
+        _catCount.value?.let { prefsEditor.putInt(KEY_CAT_COUNT, it) }
+        _crowCount.value?.let { prefsEditor.putInt(KEY_CROW_COUNT, it) }
+        prefsEditor.apply()
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("viewModel","viewModelCleared")
     }
 
 }
